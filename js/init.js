ea3727513a0c71d83cb3fc9bc1038510ff655965
JesusMejia3VC51 <75956851+JesusMejia3VC51@users.noreply.github.com>
2021-01-15 23:59:12 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,8 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyCRyqyQBbsLDb2x4sCLTe4FF8P5mCBAm7A",
+    authDomain: "tenis3cv51.firebaseapp.com",
+    projectId: "tenis3cv51",
+    storageBucket: "tenis3cv51.appspot.com",
+    messagingSenderId: "366627943996",
+    appId: "1:366627943996:web:5c0e7c69f4e0f5c87ef539"
+  });
