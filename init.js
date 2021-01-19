35f51f63bd114282c80093fb8f0a9b7b1b4ec783
JesusMejia3VC51 <75956851+JesusMejia3VC51@users.noreply.github.com>
2021-01-18 18:07:44 -0600

    Add files via upload

---
+++ init.js
@@ -0,0 +1,37 @@
+(function(){
+const config = {
+    apiKey: "AIzaSyCRyqyQBbsLDb2x4sCLTe4FF8P5mCBAm7A",
+    authDomain: "tenis3cv51.firebaseapp.com",
+    projectId: "tenis3cv51",
+    storageBucket: "tenis3cv51.appspot.com",
+    messagingSenderId: "366627943996",
+    appId: "1:366627943996:web:5c0e7c69f4e0f5c87ef539"
+  };
+  firebase.initializeApp(config);
+   
+   const txtEmail = document.getElementById('txtEmail')
+   const txtPassword = document.getElementById('txtPassword')
+   const btnLogin = document.getElementById('btnLogin')
+   const btnSignUp = document.getElementById('btnSignUp')
+   const btnLogout = document.getElementById('btnLogout')
+
+   btnLogin.addEventListenner('click', e => {
+   	const email = txtEmail.value;
+   	const pass = txtPassword.value;
+   	const auth = firebase.auth();
+
+   	const promise = auth.signInWithEmailAndPassword(email, pass);
+   	promise.catch(e => console.log(e.message));
+   });
+
+   btnSignUp.addEventListenner('click', e => {
+   	const email = txtEmail.value;
+   	const pass = txtPassword.value;
+   	const auth = firebase.auth();
+
+   	const promise = auth.createUserWithEmailAndPassword(email, pass);
+   	promise.catch(e => console.log(e.message));
+   });
+
+}()); 
+   
