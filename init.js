e790fcf11f4f2a24f79de527269118720c5584fd
JesusMejia3VC51 <75956851+JesusMejia3VC51@users.noreply.github.com>
2021-01-18 18:57:36 -0600

    Update init.js

--- init.js
+++ init.js
@@ -1,14 +1,4 @@
 (function(){
-const config = {
-    apiKey: "AIzaSyCRyqyQBbsLDb2x4sCLTe4FF8P5mCBAm7A",
-    authDomain: "tenis3cv51.firebaseapp.com",
-    projectId: "tenis3cv51",
-    storageBucket: "tenis3cv51.appspot.com",
-    messagingSenderId: "366627943996",
-    appId: "1:366627943996:web:5c0e7c69f4e0f5c87ef539"
-  };
-  firebase.initializeApp(config);
-   
    const txtEmail = document.getElementById('txtEmail')
    const txtPassword = document.getElementById('txtPassword')
    const btnLogin = document.getElementById('btnLogin')
@@ -33,4 +23,4 @@ const config = {
    	promise.catch(e => console.log(e.message));
    });
 
-}());
+}()); 
